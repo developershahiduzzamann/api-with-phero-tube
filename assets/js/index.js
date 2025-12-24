5de8267272fec547ff31c560241eeda21cdc5741
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-12-24 22:35:12 +0600

    added the new line

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,21 +15,41 @@ function  loadCard (){
     })
 }
 
+const musicVideo = (id)=>{
+    const url =`https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => loadMainCard(data.category))
+};
+
+
 function displayCatagories (catagoris){
     const catagoryContainer = document.getElementById("catagori-container");
     for(const cat of catagoris){
         const catagoyiDiv = document.createElement("div");
         catagoyiDiv.innerHTML = `
-        <button class="btn bg-[#D3D3D3] text-base inter font-medium hover:bg-red-500 hover:text-white">${cat.category}</button>
+        <button onclick ="musicVideo(${cat.category_id})" class="btn bg-[#D3D3D3] text-base inter font-medium hover:bg-red-500 hover:text-white">${cat.category}</button>
         `;
         catagoryContainer.append(catagoyiDiv)
     }
 }
 
-function loadMainCard(cards){
-    const videoContainer = document.getElementById("video-container");
-    for(const card of cards){
+
+const  loadMainCard=(cards)=>{
+    const videoContainer = document.getElementById("video-container");  
+        videoContainer.innerHTML = "";
+    if(cards.length === 0){
+        videoContainer.innerHTML = `
+            <div class="flex items-center flex-col mt-36">
+                <img src="./assets/images/Icon.png" alt="">
+                <h1 class="inter font-bold text-[32px]">Oops!! Sorry, There is no <span class="block text-center">content here</span></h1>
+            </div>
+        `;
+    return;
+    }
+        cards.forEach((card) =>{
         const videoCard = document.createElement("div")
+        
         videoCard.innerHTML = `
             <div class="w-[312px] h-[325px]">
                 <div class="relative">
@@ -40,7 +60,7 @@ function loadMainCard(cards){
                     <div class="h-[40px] w-[40px] object-cover rounded-full">
                         <img class="rounded-full h-[40px] w-[40px] object-cover" src="${card.authors[0].profile_picture}" alt="">
                     </div>
-                    <h2 class="font-bold text-base w-[260px]">Building a Winning UX Strategy Using the Kano Model</h2>
+                    <h2 class="font-bold text-base w-[260px]">${card.title}</h2>
                 </div>
                 <div class="flex items-center gap-2 mt-3 ml-12">  
                     <h3 class="inter text-sm">${card.authors[0].profile_name}</h3>
@@ -51,10 +71,15 @@ function loadMainCard(cards){
                 </div>
             </div>
         `
-        videoContainer.append(videoCard)
-    }
+        videoContainer.appendChild(videoCard)
+        })
+            
+       
 }
 
-
-
 loadCatagories ()
+
+
+
+
+
